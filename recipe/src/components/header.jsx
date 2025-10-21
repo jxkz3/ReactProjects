3d7401771315987d706daae2b28b16223bf17854
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-10-21 15:44:45 +0530

    new project setup

---
+++ recipe/src/components/header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+  return (
+    <header>
+        <img src="/icon.png" alt="Anime Picker Logo" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+      <h1>Anime Picker</h1>
+    </header>
+  );
+}
+
+export default Header;
