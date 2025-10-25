e451de561488971fe8767a14d86fbaacdc27f9f0
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-10-25 16:48:06 +0530

    list heading

--- recipe/src/components/Main.jsx
+++ recipe/src/components/Main.jsx
@@ -1,19 +1,28 @@
 import List from "./List.jsx";
+import React, { useState } from "react";
 
 function Main() {
-  const genre = ["action", "sc-fi", "Fantasy"];
+  const [genre, setGenre] = useState([]);
 
-  function Submit(event) {
-    event.preventDefault();
+  function addGenre(formData) {
+    const newGenre = formData.get("genre");
+    setGenre((g) => [...g, newGenre]);
+  }
+
+  function genreHeading() {
+    return genre.length > 0 ? "genre on Hand" : null;
   }
 
   return (
     <main>
-      <form className="genre-form" onSubmit={Submit}>
-        <input type="text" placeholder="favourite Genre..." name="Gener" />
+      <form className="genre-form" action={addGenre}>
+        <input type="text" placeholder="favourite Genre..." name="genre" />
         <button>Add</button>
       </form>
-      <List best={genre} />
+      <section>
+        <h2>{genreHeading()}</h2>
+        <List best={genre} />
+      </section>
     </main>
   );
 }
