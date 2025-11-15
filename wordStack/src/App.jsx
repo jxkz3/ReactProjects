3db28a0aca24dfa5c8dcaf29c9409570fdc3e271
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-15 20:30:38 +0530

    Initial Quick update

--- wordStack/src/App.jsx
+++ wordStack/src/App.jsx
@@ -2,16 +2,19 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="main-layer">
-      <div className="second-layer">
-        <h1> Word Stack</h1>
+    <div className="main-ct">
+      <div className="head-ct">
+        <h1> Word Stack Game</h1>
         <p>
           Guess the word under 7 attemepts to keep the programming community
           alive
         </p>
+        <div className="status-ct">
+          <h1>You won</h1> <p> welldon </p>
+        </div>
       </div>
-      <div className="third-layer">
-        <h1> jas</h1>
+      <div className="items-ct">
+        <h1> items </h1>
         <button>one</button>
         <button>one</button>
         <button>one</button>
@@ -21,7 +24,34 @@ function App() {
         <button>one</button>
         <button>one</button>
       </div>
-      <div className="keyboard-ct"></div>
+      <div className="word-ct">
+        <button> empty</button>
+      </div>
+      <div className="keyboard-ct">
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+        <button>k</button>
+      </div>
     </div>
   );
 }
