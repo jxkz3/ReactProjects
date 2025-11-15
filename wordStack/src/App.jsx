6fa16a0ea1dd948fe225bebc22c77a362aba8b4c
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-15 23:45:00 +0530

    feat: upadte all feature

--- wordStack/src/App.jsx
+++ wordStack/src/App.jsx
@@ -1,6 +1,39 @@
+import { useState } from "react";
 import "./App.css";
+import { languages } from "./languages.js";
 
 function App() {
+  const words = "words";
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const letters = words.split("").map((letter, id) => {
+    return (
+      <div className="letter" key={id}>
+        {letter.toUpperCase()}
+      </div>
+    );
+  });
+
+  const keyBoard = alphabet.split("").map((alph, index) => {
+    return (
+      <button className="keyboard-btn" key={index}>
+        {alph}
+      </button>
+    );
+  });
+
+  const items = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <button className="lang-btn" key={lang.name} style={styles}>
+        {lang.name}
+      </button>
+    );
+  });
+
   return (
     <div className="main-ct">
       <div className="head-ct">
@@ -13,45 +46,12 @@ function App() {
           <h1>You won</h1> <p> welldon </p>
         </div>
       </div>
-      <div className="items-ct">
+      <div>
         <h1> items </h1>
-        <button>one</button>
-        <button>one</button>
-        <button>one</button>
-        <button>one</button>
-        <button>one</button>
-        <button>one</button>
-        <button>one</button>
-        <button>one</button>
+        <div className="items-ct">{items}</div>
       </div>
-      <div className="word-ct">
-        <button> empty</button>
-      </div>
-      <div className="keyboard-ct">
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-        <button>k</button>
-      </div>
+      <div className="word-ct">{letters}</div>
+      <div className="keyboard-ct">{keyBoard}</div>
     </div>
   );
 }
