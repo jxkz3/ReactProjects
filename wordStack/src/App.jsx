74080422ed56a4d88225457b93c0cbd5fec828fa
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-15 19:44:20 +0530

    Initial setup

--- wordStack/src/App.jsx
+++ wordStack/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="main-layer">
+      <div className="second-layer">
+        <h1> Word Stack</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Guess the word under 7 attemepts to keep the programming community
+          alive
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="third-layer">
+        <h1> jas</h1>
+        <button>one</button>
+        <button>one</button>
+        <button>one</button>
+        <button>one</button>
+        <button>one</button>
+        <button>one</button>
+        <button>one</button>
+        <button>one</button>
+      </div>
+      <div className="keyboard-ct"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
