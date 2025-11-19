fbcf651e2b2e7f84a109a042f66eb8f3efba5db1
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-19 22:34:42 +0530

    Feat : Update react router dom

--- bike-life/src/App.jsx
+++ bike-life/src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ bike-life/src/pages/About.jsx
@@ -0,0 +1,5 @@
+function About() {
+  return <h1> it aboute page</h1>;
+}
+
+export default About;
