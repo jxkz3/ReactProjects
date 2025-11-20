32e43f51efcb55e61556d82cf7e0b8dd79ef5b32
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-20 10:07:42 +0530

    Quick update

--- bike-life/src/pages/About.jsx
+++ bike-life/src/pages/About.jsx
@@ -1,5 +1,20 @@
 function About() {
-  return <h1> it aboute page</h1>;
+  return (
+    <div>
+      <div>
+        <img alt="image"></img>
+      </div>
+      <h2>Dont Waste money on scamms</h2>
+      <p>
+        Our mission is safe gaurd our customers from potenetiol scamm rentiing
+        become professional well mainteinded rental of bilke
+      </p>
+      <div>
+        <h5>Your desitination is waiting , Your bike is ready</h5>
+        <button>explore our bikes</button>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- bike-life/src/pages/Home.jsx
+++ bike-life/src/pages/Home.jsx
@@ -1,3 +1,23 @@
 export default function Home() {
-  return <h1> we are in home</h1>;
+  return (
+    <>
+      <header>
+        <h1> Bike lIfe</h1>
+      </header>
+      <main>
+        <div>
+          <h2> You got the travle plan go to bike life</h2>
+          <p>
+            Add adveneture to the life by joining Bike life rent the perfect
+            bike without any scamm
+          </p>
+          <button>Find you r Value</button>
+        </div>
+      </main>
+
+      <footer>
+        <p> @2025 Bike Life</p>
+      </footer>
+    </>
+  );
 }
