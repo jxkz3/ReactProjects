0c1cf49ca2f65c8af05e1a12bfa64ba0e8c20ca8
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-12-03 16:16:53 +0530

    style: add css to header and body

--- jasik-portfolio/src/components/Header.jsx
+++ jasik-portfolio/src/components/Header.jsx
@@ -2,17 +2,19 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
+    <header className="head-ct">
       <Link to="/">
         <h1>WebDev</h1>
       </Link>
-      <nav>
-        <Link to="/"> Home</Link>
-        <NavLink to="contacts">Contacts</NavLink>
-        <NavLink to="skills">Skills</NavLink>
-        <NavLink to="projects">Projects</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
+      <div className="head-nav-ct">
+        <nav className="head-nav">
+          <Link to="/"> Home</Link>
+          <NavLink to="contacts">Contacts</NavLink>
+          <NavLink to="skills">Skills</NavLink>
+          <NavLink to="projects">Projects</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
