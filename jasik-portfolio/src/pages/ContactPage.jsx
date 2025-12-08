b29ed53b00946f324361d2e727ecfbefb19398e2
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-12-08 22:31:23 +0530

    refactor: style contact page

--- jasik-portfolio/src/pages/ContactPage.jsx
+++ jasik-portfolio/src/pages/ContactPage.jsx
@@ -1,7 +1,8 @@
 export default function ContactPage() {
   return (
-    <div>
+    <div className="contact-ct">
       <h1>Contact Me</h1>
+
       <p>
         <strong>Name:</strong> JASIK MJ
       </p>
@@ -13,24 +14,53 @@ export default function ContactPage() {
       </p>
       <p>
         <strong>Email:</strong>{" "}
-        <a href="mailto:jasikmj02@gmail.com">jasikmj02@gmail.com</a>
+        <a
+          href="mailto:jasikmj02@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          jasikmj02@gmail.com
+        </a>
       </p>
       <p>
         <strong>Portfolio:</strong>{" "}
-        <a href="https://jasik-portfolio.netlify.app/">
+        <a
+          href="https://jasik-portfolio.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           https://jasik-portfolio.netlify.app/
         </a>
       </p>
       <p>
         <strong>GitHub:</strong>{" "}
-        <a href="https://github.com/jxkz3">https://github.com/jxkz3</a>
+        <a
+          href="https://github.com/jxkz3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/jxkz3
+        </a>
       </p>
       <p>
         <strong>LinkedIn:</strong>{" "}
-        <a href="https://www.linkedin.com/in/jxkz/">
+        <a
+          href="https://www.linkedin.com/in/jxkz/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           https://www.linkedin.com/in/jxkz/
         </a>
       </p>
+
+      <a
+        className="libutton"
+        href="https://www.linkedin.com/in/jxkz/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Check my LinkedIn
+      </a>
     </div>
   );
 }
